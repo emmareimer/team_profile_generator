1d38dcc3297f3a496c5be4cbd8a588133a69f4a1
emma@beinproximity.com <emma@beinproximity.com>
2022-03-03 15:49:32 -0700

    working on finishing functions and styling

--- index.js
+++ index.js
@@ -21,96 +21,137 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const parser = require("./parse");
 
-
 let team = {
-    manager: '',
-    engineers: [],
-    interns: []
-}
-
+  manager: "",
+  engineers: [],
+  interns: [],
+};
 
 const addManager = () => {
-    inquirer.prompt([
-        {
-          type: "input",
-          name: "teamManagerName",
-          message: "What is your team manager's name?",
-        },
-        {
-          type: "input",
-          name: "employeeID",
-          message: "What is your employee ID?",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "What is your email address?",
-        },
-        {
-          type: "input",
-          name: "officeNumber",
-          message: "What is your office number?",
-        },
-      ]).then((answers) => {
-          team.manager = answers;
-            showMenu();
-      
-        
-      });
-}
-
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "teamManagerName",
+        message: "What is your team manager's name?",
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+    ])
+    .then((answers) => {
+      team.manager = answers;
+      showMenu();
+    });
+};
 
 const addEngineer = () => {
-inquirer.prompt([
-    {
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "engineerName",
         message: "What is the engineer's name?",
       },
-]).then(answers => {
-    team.engineers.push(answers)
-    showMenu();
-})
-}
-
+      {
+        type: "input",
+        name: "engineerID",
+        message: "What is the engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGithubUsername",
+        message: "What is the engineer's Github username?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's Github link?",
+      },
+    ])
+    .then((answers) => {
+      team.engineers.push(answers);
+      showMenu();
+    });
+};
 
 const addIntern = () => {
-
-}
-
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is the intern's school?",
+      },
+    ])
+    .then((answers) => {
+      team.interns.push(answers);
+      showMenu();
+    });
+};
 
 // Gets called after every person gets added
 const showMenu = () => {
-    inquirer.prompt( [
-        {
-          type: "list",
-          name: "menuChoice",
-          message:
-            "Would you like to add an engineer or an intern or to finish building your team?",
-          choices: ["Add an Engineer", "Add an Intern", "Finish Building my Team"],
-        },
-    ]).then((choice) => {
-        const {menuChoice} = choice;
-        if (menuChoice == "Add an Engineer") {
-            addEngineer();
-        }
-        else if (menuChoice == "Add an Intern") {
-            addIntern();
-        }
-        else {
-            renderHtml();
-        }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuChoice",
+        message:
+          "Would you like to add an engineer or an intern or to finish building your team?",
+        choices: [
+          "Add an Engineer",
+          "Add an Intern",
+          "Finish Building my Team",
+        ],
+      },
+    ])
+    .then((choice) => {
+      const { menuChoice } = choice;
+      if (menuChoice == "Add an Engineer") {
+        addEngineer();
+      } else if (menuChoice == "Add an Intern") {
+        addIntern();
+      } else {
+        renderHtml();
+      }
     });
-}
-
+};
 
 const renderHtml = () => {
-    const data = parser(team)
-        // Appends data to log.md file/creates file if one is not already created
-        fs.appendFile("log.html", data, (err) =>
-          // if error, console log error, else console log commit logged
-          err ? console.error(err) : console.log("Commit logged!")
-        );
-}
+  const data = parser(team);
+  // Appends data to log.md file/creates file if one is not already created
+  fs.appendFile("log.html", data, (err) =>
+    // if error, console log error, else console log commit logged
+    err ? console.error(err) : console.log("Commit logged!")
+  );
+};
 
 addManager();
--- parse.js
+++ parse.js
@@ -1,4 +1,4 @@
-const parser = ({manager, engineers}) => {
+const parser = ({manager, engineers, interns}) => {
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -6,38 +6,71 @@ const parser = ({manager, engineers}) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./style.css">
     <title>Team Profiles</title>
     </head>
     <body>
 
-    ${manager.teamManagerName}
-    ${manager.employeeID}
-    ${manager.email}
-    ${manager.officeNumber}
-    
-    ====
+    <nav>
+    <h1>My Team</h1>
+    </nav>
+    <main>
+
+    <div class="card">
+    <h2>${manager.teamManagerName}</h2>
+    <h3>Manager</h3>
+        <section class="card-items">
+        <h4>ID: ${manager.employeeID}</h4>
+        <h4>Email: <a href="mailto: ${manager.email}">${manager.email}</a></h4>
+        <h4>Office Number: ${manager.officeNumber}</h4>
+        </section>
+    </div>
 
     ${
         engineers.map(element => {
             return renderEngineer(element)
         })
     }
+
+    ${
+        interns.map(element => {
+            return renderIntern(element)
+        })
+    }
+
+    </main>
     </body>
     </html>
     `;
 };
 
 
-const renderEngineer = ({engineerName}) => {
+const renderEngineer = ({engineerName, engineerID, engineerEmail, engineerGithub, engineerGithubUsername}) => {
     return `
     <div class="card">
-    Name: ${engineerName}
+        <h2>${engineerName}</h2>
+        <h3>Engineer</h3>
+        <h4>ID: ${engineerID}</h4>
+        <h4>Email: <a href="mailto: ${engineerEmail}">${engineerEmail}</a></h4>
+        <h4>Github: <a href ="${engineerGithub}" target="_blank">${engineerGithubUsername}</a></h4>
     </div>
     `
 }
 
 // Create const for interns
+const renderIntern = ({internName, internEmail, internSchool}) => {
+    return `
+    <div class="card">
+        <h2>${internName}</h2>
+        <h3>Intern</h3>
+        <h4>Email: <a href="mailto: ${internEmail}">${internEmail}</a></h4>
+        <h4>School: ${internSchool}</h4>
+    </div>
+    `
+}
 
 
 // Connects parsemd to index.js
